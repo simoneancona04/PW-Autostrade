f22108724e56d85b61f0f9230f4f02a613e24571
simoneancona04 <simone.ancona.2004@calvino.edu.it>
2022-04-21 18:32:46 +0200

    db

--- Autostrade/Program.cs
+++ Autostrade/Program.cs
@@ -10,16 +10,18 @@ namespace Autostrade
         /// </summary>
         /// 
 
-        const string cFile = "db/44000";
-        const string uFile = "db/44001";
-        const string cFile1 = "db/44002";
-        const string cFile2 = "db/44003";
+        const string cFile = "db/44000";    // vetture
+        const string uFile = "db/44001";    // utenti
+        const string mFile = "db/44002";    // manutenzioni
+        const string lFile = "db/44003";    // patenti
 
         [STAThread]
         static void Main()
         {
             var cars = new Process();
             var users = new Process();
+            var mai = new Process();
+            var license = new Process();
 
             cars.StartInfo.FileName = cFile + "/mtdb.exe";
             cars.StartInfo.WorkingDirectory = cFile;
@@ -31,9 +33,20 @@ namespace Autostrade
             users.StartInfo.CreateNoWindow = true;
             users.Start();
 
+            users.StartInfo.FileName = mFile + "/mtdb.exe";
+            users.StartInfo.WorkingDirectory = mFile;
+            users.StartInfo.CreateNoWindow = true;
+            users.Start();
+
+            users.StartInfo.FileName = lFile + "/mtdb.exe";
+            users.StartInfo.WorkingDirectory = lFile;
+            users.StartInfo.CreateNoWindow = true;
+            users.Start();
+
             var carDatabase = new MTDBHandler("localhost", 44000);
             var userDatabase = new MTDBHandler("localhost", 44001);
-            //var carDatabase = new MTDBHandler("localhost", 44000);
+            var maintDatabase = new MTDBHandler("localhost", 44002);
+            var licenseDatabase = new MTDBHandler("localhost", 44003);
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
